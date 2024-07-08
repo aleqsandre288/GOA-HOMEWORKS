c547ece8994e24fc0293ee0fd801ead6049c1aa6
aleqsandre288 <alexdolidze674@gmail.com>
2024-07-08 05:53:48 -0700

    second commit

---
+++ LEVEL 1/HOMEWORK/python/homework.py
@@ -0,0 +1,11 @@
+name = "aleqsandre"
+print(name)
+
+age = 15
+print(age)
+number = 20
+number2 = 50
+print(number + number2)
+name2 = "sandro"
+name3 = "giorgi"
+print(name2 +" "+ "and" + " " + name3)
