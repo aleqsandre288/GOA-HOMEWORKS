c9b65ced30d5cec570f23870d73b70f1ef12f409
aleqsandre288 <alexdolidze674@gmail.com>
2024-08-26 04:38:18 -0700

    eighth

---
+++ level11/classwork/classwork.py
@@ -0,0 +1,30 @@
+#1
+for i in range(1, 100):
+   print(i)
+
+#2
+for i in range(5, 50, 5):
+    print(i)
+
+#3
+for i in range(50):
+    print("GOA BEST")
+
+#4
+for i in range(25):
+    print(i + 2)
+
+#5
+for i in range(100):
+    print(i * 2)
+
+#6
+for i in range(1, 500, 10):
+    print(i * 25)
+
+
+#7
+name = input("ENTER YOUR NAME:")
+
+for i in range(15):
+   print(str(i) + name)
