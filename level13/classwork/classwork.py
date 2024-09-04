470348c3c1800103c0ecbf3fefb3af74ecce7fc0
aleqsandre288 <alexdolidze674@gmail.com>
2024-09-04 00:30:40 -0700

    13 level

---
+++ level13/classwork/classwork.py
@@ -0,0 +1,6 @@
+for x in range(7):
+    print("goa")
+
+
+    for i in range(9):
+     print(i)
---
+++ level13/homework/homework.py
@@ -0,0 +1,23 @@
+for i in range(7):
+    print(10)
+for i in range(8):
+    print('hello')
+
+user_name= input("ENTER YOUR NAME: ")
+for i in range(5):
+    print('hello' + '  ' + user_name)
+
+num=30
+while num>9:
+    print(num)
+    num= num-1
+
+num1=50
+while num1>9:
+    print(num1)
+    num1=num1-1
+
+counter=0
+while counter < 10:
+    print("Goa")
+    counter += 1
