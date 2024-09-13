7b5e8604c6833728238aea4824b12fa577bcc326
aleqsandre288 <alexdolidze674@gmail.com>
2024-09-13 01:35:03 -0700

    14 level

---
+++ level14/homework/homework.py
@@ -0,0 +1,22 @@
+#1
+name= input("ENTER YOUR NAME: ")
+for i in range(5):
+    print("hello" + "" + name)
+#2
+for i in range(20):
+    print(i/2)
+ #3
+for i in range(15):
+    print(i*2)
+
+#4
+for i in range(10):
+    print(i*i)
+
+#5
+sum = 0
+for i in range(10):
+    sum += i
+print("Sum:", sum)
+
+
