6adeba3734e554161dd997f93cf09a01c669e973
aleqsandre288 <alexdolidze674@gmail.com>
2024-10-11 10:40:03 -0700

    18 level

---
+++ level17/homework/homework.py
@@ -0,0 +1,7 @@
+#1
+age1=int(input("ENTER YOUR AGE: "))
+age2=int(input("ENTER YOUR FRIENDS NAME: "))
+if age1 < age2:
+    print("you are smaller than your friend")
+elif age1> age2:
+    print("you are bigger than your friend")
