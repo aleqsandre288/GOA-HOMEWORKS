f20fd9ab4341f4cd4b4d9418c6f4c4cff2097f5f
aleqsandre288 <alexdolidze674@gmail.com>
2024-10-19 04:47:52 -0700

    19 level

---
+++ level19/classwork/class.py
@@ -0,0 +1,7 @@
+list= ["football" , "basketball" , "badminton " , "hockey" , "tennis"]
+print(list[2])
+
+# mutable ცვლადს მნიშვნელობის შეცვლა რო შეუძლია , immutable ცვლადს არ შეუძლია მნიშვნელობის შეცვლა.
+
+programming_academy= "goa"
+print(programming_academy[2])
