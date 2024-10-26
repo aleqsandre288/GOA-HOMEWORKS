9e7d64649a068225dcf2708b7da6e746775d67f8
aleqsandre288 <alexdolidze674@gmail.com>
2024-10-26 02:56:08 -0700

    20level

---
+++ level17/classwork/class.py
@@ -0,0 +1,7 @@
+age= int(input("ENTER YOUR AGE: "))
+if age >= 18:
+    print("you are adult")
+elif age <18:
+    print("you are underage")
+    
+    
---
+++ level17/classwork/class2.py
@@ -0,0 +1,6 @@
+password= "aleqsandre"
+guess=input("ENTER PASSWORD: ")
+if password==guess:
+    print("YOUR PASSWORD IS RIGHT")
+elif password != guess:
+    print("ITS INCCORECT")
