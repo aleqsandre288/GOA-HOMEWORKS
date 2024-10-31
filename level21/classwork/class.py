fd383754fc0eb33251404c35e55ce99b51c9b909
aleqsandre288 <alexdolidze674@gmail.com>
2024-10-31 10:48:44 -0700

    21 level

---
+++ level21/classwork/class.py
@@ -0,0 +1,20 @@
+#1)
+lis=['oga', 'basketball', 'football', 'homework', 'grades', 'youtube', 'car']
+print(lis[1:3])
+
+#2)
+print(lis[4:])
+
+#3)
+print(lis[-6:-4])
+
+#4)
+print(lis[-3:])
+
+#5)
+
+print(lis[2:-1])
+
+#6)
+
+print(lis[:])
---
+++ level21/homework/homework.py
@@ -0,0 +1,22 @@
+#1)
+
+
+list=[ "chicken" , "egg" , "tofu" , "milk"]
+print(list[1:2])
+
+#2)
+list[1] = "water"
+print(list)
+
+#3)
+my_list = ["apple", "banana", "cherry", "date", "elderberry"]
+print(my_list[:2])
+#4)
+print(my_list[:-3])
+
+
+#5)
+colors = ["red", "blue", "green", "yellow", "purple", "orange"]
+print(colors[:-2])
+
+
