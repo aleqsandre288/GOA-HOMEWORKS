70c70023bca4692f6d12c8673ad9294d96085aec
aleqsandre288 <alexdolidze674@gmail.com>
2025-05-09 02:28:54 -0700

    50level

---
+++ level50/classwork/index.js
@@ -0,0 +1,7 @@
+let h1 = document.getElementById('text')
+
+h1.style.color = "red"
+h1.style.backgroundColor = "blue"
+h1.style.fontSize = "30px"
+
+console.log(h1);
---
+++ level50/homework/index.js
@@ -0,0 +1,14 @@
+let text = document.getElementById('paragraph');
+text.style.color = "red";
+text.style.fontSize = "40px";
+text.style.fontFamily = "Arial";
+
+
+let text2 = document.getElementById('paragraph2');
+text2.style.color = "blue";
+text2.style.fontSize = "30px";
+text2.style.fontFamily = "Arial";
+
+
+console.log(text);
+console.log(text2);
